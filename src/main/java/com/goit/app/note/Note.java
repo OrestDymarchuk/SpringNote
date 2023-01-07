0cf68345c9729fc3df5af1f94d56588f136a5bbf
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2023-01-07 21:33:37 +0400

    Removed extra constructors

--- src/main/java/com/goit/app/note/Note.java
+++ src/main/java/com/goit/app/note/Note.java
@@ -7,12 +7,4 @@ public class Note {
     private long id;
     private String title;
     private String content;
-
-    public Note(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    public Note() {
-    }
 }
