5cbfe8a71e9dd69b1310b624c4c1e049dde2bb46
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2023-01-16 19:27:20 +0400

    Updated edit controller

--- src/main/java/com/goit/app/mvc/NoteController.java
+++ src/main/java/com/goit/app/mvc/NoteController.java
@@ -2,8 +2,10 @@ package com.goit.app.mvc;
 
 import com.goit.app.note.Note;
 import com.goit.app.note.NoteService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -14,7 +16,7 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class NoteController {
     private final NoteService noteService;
-    private Note noteForUpdate = new Note();
+    private Note noteToUpdate;
 
     @GetMapping("/list")
     public ModelAndView getNotes() {
@@ -48,23 +50,36 @@ public class NoteController {
     }
 
     @GetMapping("/edit")
-    public String editNote(@RequestParam long id) {
-        noteForUpdate = noteService.getById(id);
-
+    public String editNote(Model model, @RequestParam long id) {
+        noteToUpdate = noteService.getById(id);
+        model.addAttribute("note", noteToUpdate);
         return ("note/edit-note");
     }
 
     @PostMapping("/edit")
-    public RedirectView edit(@RequestParam("title") String title,
+    public RedirectView edit(Model model, @RequestParam("title") String title,
                              @RequestParam("content") String content) {
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl("/note/list");
 
-        noteForUpdate.setTitle(title);
-        noteForUpdate.setContent(content);
+        noteToUpdate.setTitle(title);
+        noteToUpdate.setContent(content);
 
-        noteService.update(noteForUpdate);
+        noteService.update(noteToUpdate);
 
         return redirectView;
+    }
+    @PostConstruct
+    public void ssA(){
+        Note note = new Note();
+        note.setTitle("Title");
+        note.setContent("Content");
+
+        Note note1 = new Note();
+        note1.setTitle("Title1");
+        note1.setContent("Content1");
+
+        noteService.add(note);
+        noteService.add(note1);
     }
 }
